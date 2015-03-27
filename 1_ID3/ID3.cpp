020df3f845371ebac630accb179860c474916186
unknown <arefinkhan@arefin.(none)>
2015-03-28 02:30:39 +0600

    bug fixed

--- 1_ID3/ID3.cpp
+++ 1_ID3/ID3.cpp
@@ -11,18 +11,93 @@
 #include <random>
 #include <math.h>
 using namespace std;
-#define MAX 1000
+#define MAX_AT 9
+#define MAX_VAR 10
+#define TEST 100
 
 #define BENIGN 0
 #define MALIGNANT 1
 
-struct data{
-    vector <int> attributes[10];
-    data(){
-        for(int i=0; i<10; i++) attributes[i].clear();
+struct data
+{
+    int attributes[MAX_AT];
+    int which_class;
+    data(int attribs[MAX_AT],int classs)
+    {
+        which_class = classs;
+        for(int i=0; i<MAX_AT; i++) attributes[i] = atribs[i];
     }
 };
-data input,training,test;
+
+vector <data> totalset,training,test;
+
+struct TreeNode
+{
+    int cl,tattr,mask;
+    TreeNode *children[MAX_VAR];
+    TreeNode()
+    {
+        cl = tattr = mask = -1;
+        for(int i=0; i<MAX_VAR; i++) children[i] = NULL;
+    }
+};
+
+int contribution[MAX_AT][MAX_VAR][2];
+
+double entropy(int positives,int negatives)
+{
+    int denom = positives + negatives;
+    if(!denom || !positives || !negatives) return 0;
+    double entro = -1 * ((double)positives/denom) * log2((double)positives/denom) - ((double)negatives/denom) * log2((double)negatives/denom);
+    return entro;
+}
+
+double entropy_calculator(vector <data> &trainset, int target_attribute)
+{
+    int mat[MAX_VAR][2];
+    memset(mat, 0, sizeof(mat));
+    for(int i=0; i<trainset.size(); i++)
+    {
+        int val = trainset[i].attributes[target_attribute];
+        int cls = trainset[i].which_class;
+        mat[val][cls]++;
+    }
+    double entro = 0.0;
+    int positives,negatives;
+    for(int i=0; i<MAX_ATTR_V; i++)
+    {
+        if (mat[i][0] == 0 || mat[i][1] == 0) continue;
+        positives = mat[i][0];
+        negatives = mat[i][1];
+        entro += entropy(positives,negatives);
+    }
+    entro /= trainset.size();
+    return entropy;
+}
+
+int getTargetAttribute(vector < data > &trainset, int mask)
+{
+    int i;
+    int clas[2] = {0};
+    double negatives,positives;
+    for(i=0; i<trainset.size(); i++)
+    {
+        clas[trainset[i].which_class]++;
+    }
+    double entro = 0.0;
+    negatives = (double)clas[0];
+    positives = (double)clas[1];
+    int sub_total = negatives + positives;
+    entro -=  (negatives/sub_total) * log2(negatives/sub_total) + (positives/sub_total) * log2(positives/sub_total);
+    vector < pair < double,int > > Gain;
+    for(i=0; i<MAX_AT; i++)
+    {
+        if (mask&(1<<i)) continue;
+        Gain.push_back(make_pair(entro - entropy_calculator(trainset,i), i));
+    }
+    sort (Gain.begin(), Gain.end());
+    return G.back().second;
+}
 
 int total_dataset,training_dataset,test_dataset;
 vector < pair<double,int> > Gain;
