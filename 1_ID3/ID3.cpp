a2d0aaa75a34b191aafdf42906ac8d30067b134e
unknown <arefinkhan@arefin.(none)>
2015-03-29 00:47:50 +0600

    ID3 final working code

--- 1_ID3/ID3.cpp
+++ 1_ID3/ID3.cpp
@@ -10,240 +10,208 @@
 #include <set>
 #include <random>
 #include <math.h>
+#include <ctime>
 using namespace std;
-#define MAX_AT 9
-#define MAX_VAR 10
 #define TEST 100
+#define MAX_ATTR 9
+#define MAX_VAR 10
 
 #define BENIGN 0
 #define MALIGNANT 1
 
-int attribs[MAX_AT];
-
-struct data
+class einfo
 {
-    int attributes[MAX_AT];
-    int which_class;
-    data(int attribs[MAX_AT],int classs)
+public:
+    int attributes[MAX_ATTR];
+    bool clas;
+    einfo(int _attr[MAX_ATTR], bool _cl)
     {
-        which_class = classs;
-        for(int i=0; i<MAX_AT; i++) attributes[i] = attribs[i];
+
+        for(int i=0; i<MAX_ATTR; i++) attributes[i] = _attr[i];
+        clas = _cl;
     }
 };
 
-vector <data> totalset,training,test;
+int contribution[MAX_ATTR][MAX_VAR][2];
+vector < einfo > data, train, test;
 
-struct TreeNode
+class TreeNode
 {
-    int cl,tattr,mask;
-    TreeNode *children[MAX_VAR];
+public:
+    int clas, targetattr, attribmask;
+    TreeNode *child[MAX_VAR];
     TreeNode()
     {
-        cl = tattr = mask = -1;
-        for(int i=0; i<MAX_VAR; i++) children[i] = NULL;
+        int i;
+        for (i=0; i<MAX_VAR; i++) child[i] = NULL;
+        targetattr = clas = attribmask = -1;
     }
 };
 
-int contribution[MAX_AT][MAX_VAR][2];
-
-double entropy(int positives,int negatives)
+double calcEntropy(vector < einfo > &trainset, int targetattr)
 {
-    int denom = positives + negatives;
-    if(!denom || !positives || !negatives) return 0;
-    double entro = -1 * ((double)positives/denom) * log2((double)positives/denom) - ((double)negatives/denom) * log2((double)negatives/denom);
-    return entro;
-}
-
-double entropy_calculator(vector <data> &trainset, int target_attribute)
-{
-    int mat[MAX_VAR][2];
-    memset(mat, 0, sizeof(mat));
-    for(int i=0; i<trainset.size(); i++)
-    {
-        int val = trainset[i].attributes[target_attribute];
-        int cls = trainset[i].which_class;
-        mat[val][cls]++;
-    }
-    double entro = 0.0;
-    int positives,negatives;
-    for(int i=0; i<MAX_VAR; i++)
-    {
-        if (mat[i][0] == 0 || mat[i][1] == 0) continue;
-        positives = mat[i][0];
-        negatives = mat[i][1];
-        entro += entropy(positives,negatives);
-    }
-    entro /= trainset.size();
-    return entro;
-}
-
-double calcEntropy(vector <data> &trainset, int tattr)
-{
-    int cnt[MAX_VAR][2];
+    int contrib[MAX_VAR][2];
     int i;
-    memset(cnt, 0, sizeof(cnt));
-    for (i=0; i<trainset.size(); ++i)
+    memset(contrib, 0, sizeof(contrib));
+    for (i=0; i<trainset.size(); i++)
     {
-        ++cnt[trainset[i].attributes[tattr]][trainset[i].which_class];
+        contrib[trainset[i].attributes[targetattr]][trainset[i].clas]++;
     }
-    double entropy = 0.0, p,n;
-    for (i=0; i<MAX_VAR; ++i)
+    double entropy = 0.0, positives,negatives;
+    for (i=0; i<MAX_VAR; i++)
     {
-        if (cnt[i][0] == 0 || cnt[i][1] == 0) continue;
-        n = cnt[i][0] / (double) (cnt[i][0] + cnt[i][1]);
-        p = cnt[i][1] / (double) (cnt[i][0] + cnt[i][1]);
-        entropy -=  (n * log10(n) + p * log10(p)) * (cnt[i][0] + cnt[i][1]);
+        if (contrib[i][0] == 0 || contrib[i][1] == 0) continue;
+        negatives = contrib[i][0] / (double) (contrib[i][0] + contrib[i][1]);
+        positives = contrib[i][1] / (double) (contrib[i][0] + contrib[i][1]);
+        entropy -=  (negatives * log10(negatives) + positives * log10(positives)) * (contrib[i][0] + contrib[i][1]);
     }
-    entropy /= log10(2.0);
+    entropy = entropy / log10(2.0);
     entropy /= trainset.size();
     return entropy;
 }
 
-int getTargetAttribute(vector < data > &trainset, int mask)
+bool isAttributeUsed(int attribmask,int k)
+{
+    return attribmask & (1<<k);
+}
+
+int findTarAttr(vector < einfo > &trainset, int attribmask)
 {
     int i;
-    int cnt[2] = {0};
-    double n,p;
-    for (i=0; i<trainset.size(); ++i) ++cnt[trainset[i].which_class];
+    int contrib[2] = {0};
+    double negatives,positives;
+    for (i=0; i<trainset.size(); i++) contrib[trainset[i].clas]++;
     double entropy = 0.0;
-    n = cnt[0] / (double) (cnt[0] + cnt[1]);
-    p = cnt[1] / (double) (cnt[0] + cnt[1]);
-    entropy -=  n * log10(n) + p * log10(p);
+    negatives = contrib[0] / (double) (contrib[0] + contrib[1]);
+    positives = contrib[1] / (double) (contrib[0] + contrib[1]);
+    entropy -=  negatives * log10(negatives) + positives * log10(positives);
     entropy /= log10(2.0);
-    vector < pair <double,int> > G;
-    for (i=0; i<MAX_AT; ++i)
+    vector < pair < double , int > > G;
+    for (i=0; i<MAX_ATTR; i++)
     {
-        if (mask&(1<<i)) continue;
+        if (isAttributeUsed(attribmask,i)) continue;
         G.push_back(make_pair(entropy - calcEntropy(trainset,i), i));
     }
     sort (G.begin(), G.end());
-    printf("7\n");
     return G.back().second;
 }
-
-TreeNode *ID3(vector <data> &trainset, int mask)
+TreeNode *createTree(vector < einfo > &trainset, int attribmask)
 {
-    int mat[2] = {0};
-    int i;
-    for (i=0; i<trainset.size(); ++i)
+    int contrib[2] = {0}, i;
+    for (i=0; i<trainset.size(); i++) contrib[trainset[i].clas]++;
+    if (contrib[0] == 0 || contrib[1] == 0 || (attribmask+1 == (1<<MAX_ATTR)))
     {
-        data d = trainset[i];
-        mat[d.which_class]++;
-    }
-    if (mat[0] == 0 || mat[1] == 0 || (mask+1 == (1<<MAX_AT)))
-    {
-        TreeNode *node;
-        if(mat[0] > mat[1])
-        {
-            node->cl = BENIGN;
-        }
-        else node->cl = MALIGNANT;
+        TreeNode *node = new TreeNode();
+        node -> clas = (contrib[0] > contrib[1]) ? BENIGN:MALIGNANT;
         return node;
     }
-    int target;
-    TreeNode *node;
-    printf("5\n");
-    node->tattr = target = getTargetAttribute(trainset, mask);
-    printf("6\n");
-    node->mask = mask | (1<<target);
-    vector <data> V[MAX_VAR];
-    for(i=0; i<trainset.size(); i++)
-    {
-        V[trainset[i].attributes[target]].push_back(trainset[i]);
-    }
-    for(i=0; i<MAX_VAR; i++)
+    int targetattr;
+    TreeNode *node = new TreeNode();
+    targetattr = (node -> targetattr) = findTarAttr(trainset, attribmask);
+    node -> attribmask = attribmask | (1<<targetattr);
+    vector < einfo > V[MAX_VAR];
+    for (i=0; i<trainset.size(); i++) V[trainset[i].attributes[targetattr]].push_back(trainset[i]);
+    for (i=0; i<MAX_VAR; i++)
     {
         if (V[i].size() == 0)
         {
-            printf("3\n");
-            node->children[i] = new TreeNode;
-            int positives = contribution[target][i][0];
-            int negatives = contribution[target][i][1];
-            if(positives > negatives)
-            {
-                node->children[i]->cl = BENIGN;
-            }
-            else node->children[i]->cl = MALIGNANT;
-            printf("4\n");
+            node -> child[i] = new TreeNode();
+            int n = contribution[targetattr][i][0], p = contribution[targetattr][i][1];
+            node -> child[i] -> clas = (n > p) ? BENIGN:MALIGNANT;
         }
-        else node->children[i] = ID3(V[i], node->mask);
+        else node -> child[i] = createTree(V[i], node -> attribmask);
     }
     return node;
 }
-
-
-int result(TreeNode *node,data d)
+bool runTest(TreeNode *node, einfo e)
 {
-    if(node->cl > -1) return node->cl;
-    while(node->children[d.attributes[node->tattr]]!=NULL);
-    return result(node->children[d.attributes[node->tattr]],d);
+    if ((node -> clas) != -1) return node->clas;
+    if (node->child[e.attributes[node->targetattr]] == NULL) while(1);
+    return runTest(node->child[e.attributes[node->targetattr]], e);
 }
 
+double getPrecision(int TP,int FP)
+{
+    return (double)TP / (TP+FP);
+}
+double getRecall(int TP,int FN)
+{
+    return (double)TP / (TP+FN);
+}
+double getGmean(int TP,int TN,int FP,int FN)
+{
+    double d1 = getRecall(TP,FN);
+    double d2 = (double)TN/(FP+TN);
+    return sqrt(d1*d2);
+}
 void init()
 {
-    int i,clas;
-    while(scanf("%d",&attribs[0])!=EOF)
+    int attr[MAX_ATTR], i;
+    int clas;
+    while (scanf("%d", &attr[0]) != EOF)
     {
-        for(i=1; i<MAX_AT; i++)
+        for (i=1; i<MAX_ATTR; i++)
         {
-            scanf("%d",&attribs[i]);
-            attribs[i]--;
+            scanf("%d", &attr[i]);
+
         }
-        scanf("%d",&clas);
-        data d(attribs,clas);
-        totalset.push_back(d);
+        for (i=0; i<MAX_ATTR; i++) attr[i]--;
+        scanf("%d", &clas);
+        data.push_back(einfo(attr,clas));
     }
 }
-
-void calculate_contribution()
+void randomize()
 {
-    int i,j;
-    for(i=0; i<training.size(); i++)
-    {
-        for(j=0; j<MAX_AT; j++)
-        {
-            data tdata = training[i];
-            contribution[j][tdata.attributes[j]][tdata.which_class];
-        }
-    }
-}
-
-void randomize_data()
-{
-    random_shuffle(totalset.begin(),totalset.end());
+    srand(time(0));
+    random_shuffle(data.begin(), data.end());
 }
 
 int main()
 {
-    freopen("data.txt","r",stdin);
+    int i;
+    freopen("data.txt", "r", stdin);
+    freopen("output.txt","w",stdout);
     init();
-    int i,j;
-    for(i=0; i<TEST; i++)
+    int trainsize = data.size() * 0.8, t;
+    int match;
+    int TP,FP,FN,TN;
+    double sum_accuracy=0,sum_precision=0,sum_recall=0,sum_fmeasure=0,sum_gmean=0;
+    double accuracy,precision,recall,fmeasure,gmean;
+    for(int k=0; k<TEST; k++)
     {
-        training.clear();
-        test.clear();
-        randomize_data();
-        for(j=0; j<totalset.size()*0.8; j++)
+        train.clear(), test.clear();
+        randomize();
+        for (i=0; i<trainsize; i++) train.push_back(data[i]);
+        for (; i<data.size(); i++) test.push_back(data[i]);
+        memset(contribution, 0, sizeof(contribution));
+        for (t=0; t<train.size(); t++)
         {
-            training.push_back(totalset[j]);
+            for (i=0; i<MAX_ATTR; i++) contribution[i][train[t].attributes[i]][train[t].clas]++;
         }
-        for(j=totalset.size()*0.8; j<totalset.size(); j++)
+        TreeNode *root = createTree(train, 0);
+        match = 0;
+        TP = FP = FN = TN = 0;
+        for (t=0; t<test.size(); t++)
         {
-            test.push_back(totalset[j]);
+            int res = runTest(root, test[t]);
+            if(res == test[t].clas) match++;
+            if(test[t].clas == MALIGNANT && res == MALIGNANT) TP++;
+            else if(test[t].clas == MALIGNANT && res == BENIGN) FN++;
+            else if(test[t].clas == BENIGN && res == BENIGN) TN++;
+            else if(test[t].clas == BENIGN && res == MALIGNANT) FP++;
         }
-        memset(contribution,0,sizeof(contribution));
-        calculate_contribution();
-        printf("1\n");
-        TreeNode *root = ID3(training,0);
-        printf("2\n");
-        int match = 0;
-        for(j=0; j<test.size(); j++)
-        {
-            int clas = result(root,test[j]);
-            if(clas == test[j].which_class) match++;
-        }
-        printf("Test #%d, matched sample = %d, mismatched=%d\n",i,match,test.size() - match);
+        sum_precision += getPrecision(TP,FP);
+        sum_accuracy += (double)match / test.size();
+        sum_recall += getRecall(TP,FN);
+        sum_fmeasure += (2*getPrecision(TP,FP)*getRecall(TP,FN))/ (getPrecision(TP,FP)+getRecall(TP,FN));
+        sum_gmean += getGmean(TP,TN,FP,FN);
+        printf("Test %d : Matched %d, Mismatched %d\n",k+1,match,test.size()-match);
     }
-
+    puts("========");
+    printf("Accuracy=%0.3f\n",sum_accuracy/TEST);
+    printf("Precision=%0.3f\n",sum_precision/TEST);
+    printf("Recall=%0.3f\n",sum_recall/TEST);
+    printf("F-measure=%0.3f\n",sum_fmeasure/TEST);
+    printf("G-mean=%0.3f\n",sum_gmean/TEST);
     return 0;
 }
