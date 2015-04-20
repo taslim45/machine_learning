645dc19789c7f8def4e8238dcae4389fd02f1506
unknown <arefinkhan@arefin.(none)>
2015-04-20 19:51:13 +0600

    parsing half done

--- 2_DocumentClassifier/src/document/classifier/Classifier.java
+++ 2_DocumentClassifier/src/document/classifier/Classifier.java
@@ -1,8 +1,12 @@
 package document.classifier;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -29,8 +33,13 @@ public class Classifier {
 		Connection c = DBConnection.getConnection();
 		try {
 			c.setAutoCommit(false);
+			
 			stmt = c.createStatement();
 			String sql1 = "DELETE FROM " + Topics.TABLE_NAME;
+			stmt.executeUpdate(sql1);
+			stmt.close();
+			stmt = c.createStatement();
+			sql1 = "DELETE FROM " + Documents.TABLE_NAME;
 			stmt.executeUpdate(sql1);
 			stmt.close();
 			c.commit();
@@ -83,49 +92,101 @@ public class Classifier {
 	{
 		Statement stmt = null;
 		Connection c = DBConnection.getConnection();
+		PreparedStatement pstmt = null;
+		File file = new File("out.txt");
+		
 		try (BufferedReader br = new BufferedReader(new FileReader("training.data")))
 		{
+			if (!file.exists()) {
+				file.createNewFile();
+			}
 			String sCurrentLine;
 			c.setAutoCommit(false);
-			boolean isTopic,isTitle,isLocation,isStory;
-			isTopic = isTitle = isLocation = isStory = false;
+			boolean isTopic,isTitle,isLocation,isStory,blankAfterTitle,blankAfterStory;
+			isTopic = isTitle = isLocation = isStory = blankAfterTitle = false;
+			blankAfterStory = true;
+			int countBlanks = 0;
+			
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
 			while ((sCurrentLine = br.readLine()) != null) 
 			{
+				int len = sCurrentLine.length();
 				//System.out.println(sCurrentLine);
 				if(sCurrentLine.isEmpty())
 				{
-					if(isStory) isTopic = isTitle = isLocation = isStory = false;
+					countBlanks++;
+					if(isStory || countBlanks >=2) 
+					{
+						isTopic = isTitle = isLocation = isStory = false;
+					}
+					if(countBlanks >= 2) blankAfterStory = true;
+					if(isTitle) blankAfterTitle = true;
 					continue;
+				}
+				else
+				{
+					String[] currLine = splitLine(sCurrentLine);
+					countBlanks = 0;
+					if(currLine.length == 1 && !isTopic && blankAfterStory)
+					{
+						bw.write(sCurrentLine + ", ");
+						
+						isTopic = true;
+						blankAfterStory = blankAfterTitle = false;
+						long topicid = -1;
+						long documentid = -1;
+						//stmt = c.createStatement();
+						
+						StringBuilder sqlBuilder = new StringBuilder();
+						sqlBuilder.append("SELECT * FROM ").append(Topics.TABLE_NAME);
+						sqlBuilder.append(" WHERE ").append(Topics.NAME).append("=?");
+						String sql = sqlBuilder.toString();
+						pstmt = c.prepareStatement(sql);
+						pstmt.setString(1, sCurrentLine);
+						ResultSet rs = pstmt.executeQuery();
+						//stmt.close();
+						while(rs.next())
+						{
+							topicid = rs.getLong(Topics.ID);
+							break;
+						}
+						rs.close();
+						pstmt.close();
+						
+						stmt = c.createStatement();
+						sqlBuilder.setLength(0);
+						sqlBuilder.append("INSERT INTO ").append(Documents.TABLE_NAME);
+						sqlBuilder.append(" (").append(Documents.TOPICS_ID).append(")");
+						sqlBuilder.append(" VALUES ");
+						sqlBuilder.append("(").append(topicid).append(")");
+						sql = sqlBuilder.toString();
+						
+						long wow = stmt.executeUpdate(sql);
+						stmt.close();
+						//System.out.println(wow);
+					}
+					else if(sCurrentLine.charAt(len - 1) == '-' && !isLocation)
+					{
+						isLocation = true;
+					}
+					else
+					{
+						if(!blankAfterTitle)
+						{
+							// still getting title
+							isTitle = true;
+						}
+						else
+						{
+							// it must be story
+							isStory = true;
+						}
+					}
 				}
 				if(!isTopic)
 				{
-					isTopic = true;
-					long topicid = -1;
-					long documentid = -1;
-					stmt = c.createStatement();
-					StringBuilder sqlBuilder = new StringBuilder();
-					sqlBuilder.append("SELECT * FROM ").append(Topics.TABLE_NAME);
-					sqlBuilder.append(" WHERE ").append(Topics.NAME).append("='").append(sCurrentLine).append("'");
-					String sql = sqlBuilder.toString();
-					ResultSet rs = stmt.executeQuery(sql);
-					stmt.close();
-					while(rs.next())
-					{
-						topicid = rs.getLong(Topics.ID);
-						break;
-					}
 					
-					stmt = c.createStatement();
-					sqlBuilder.setLength(0);
-					sqlBuilder.append("INSERT INTO ").append(Documents.TABLE_NAME);
-					sqlBuilder.append(" (").append(Documents.TOPICS_ID).append(")");
-					sqlBuilder.append(" VALUES ");
-					sqlBuilder.append("(").append(topicid).append(")");
-					sql = sqlBuilder.toString();
-					
-					long wow = stmt.executeUpdate(sql);
-					stmt.close();
-					System.out.println(wow);
 				}
 				else if(!isTitle)
 				{
@@ -148,7 +209,10 @@ public class Classifier {
 				stmt.executeUpdate(sql);
 				stmt.close();*/
 			}
+			
 			c.commit();
+			bw.close();
+			fw.close();
 			if(c!=null) {
 				c.setAutoCommit(true); 
 				c.close();
@@ -157,5 +221,11 @@ public class Classifier {
 			e.printStackTrace();
 		}
 		System.out.println("Done");
+	}
+	
+	private static String[] splitLine(String line)
+	{
+		String[] splitStr = line.split("\\s+");
+		return splitStr;
 	}
 }
